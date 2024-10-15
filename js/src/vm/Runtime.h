dbef03b11477e76edd4940a8bfcbc6ad195326c2
Riccardo Mottola <rm@gnu.org>
2024-10-15 12:30:19 +0200

    Bug 1277278 part 3 - Rename error reporter callback to warning reporter, assert it's only used for warnings.

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -1263,8 +1263,8 @@ struct JSRuntime : public JS::shadow::Runtime,
     JSDestroyPrincipalsOp destroyPrincipals;
     JSReadPrincipalsOp readPrincipals;
 
-    /* Optional error reporter. */
-    JSErrorReporter     errorReporter;
+    /* Optional warning reporter. */
+    JS::WarningReporter warningReporter;
 
     JS::BuildIdOp buildIdOp;
 
