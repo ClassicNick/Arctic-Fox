15c572cedc08a5beb9c22e9c3edcf8f6ffd724e6
Riccardo Mottola <rm@gnu.org>
2024-10-30 09:16:51 +0100

    Bug 1264948 - Compily with Windows lack of support for noexcept keyword. r=me

--- js/src/jit/JitAllocPolicy.h
+++ js/src/jit/JitAllocPolicy.h
@@ -183,7 +183,7 @@ class AutoJitContextAlloc
 
 struct TempObject
 {
-    inline void* operator new(size_t nbytes, TempAllocator::Fallible view) noexcept {
+    inline void* operator new(size_t nbytes, TempAllocator::Fallible view) throw() {
         return view.alloc.allocate(nbytes);
     }
     inline void* operator new(size_t nbytes, TempAllocator& alloc) {
