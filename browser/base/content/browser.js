3ba759b6cb05fe719f86d801f9818fc3fd263f9c
Riccardo Mottola <rm@gnu.org>
2024-11-19 10:32:28 +0100

    Bug 1240594 - Deduplicate certerror and neterror message passing code. r=Gijs

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2776,9 +2776,9 @@ var BrowserOnClick = {
   receiveMessage: function (msg) {
     switch (msg.name) {
       case "Browser:CertExceptionError":
-        this.onAboutCertError(msg.target, msg.data.elementId,
-                              msg.data.isTopFrame, msg.data.location,
-                              msg.data.securityInfoAsString);
+        this.onCertError(msg.target, msg.data.elementId,
+                         msg.data.isTopFrame, msg.data.location,
+                         msg.data.securityInfoAsString);
       break;
       case "Browser:SiteBlockedError":
         this.onAboutBlocked(msg.data.elementId, msg.data.reason,
@@ -2839,7 +2839,7 @@ var BrowserOnClick = {
                                  uri.host, uri.port);
   },
 
-  onAboutCertError: function (browser, elementId, isTopFrame, location, securityInfoAsString) {
+  onCertError: function (browser, elementId, isTopFrame, location, securityInfoAsString) {
     let secHistogram = Services.telemetry.getHistogramById("SECURITY_UI");
 
     switch (elementId) {
@@ -2877,7 +2877,7 @@ var BrowserOnClick = {
 
         let errorInfo = getDetailedCertErrorInfo(location,
                                                  securityInfoAsString);
-        browser.messageManager.sendAsyncMessage("AboutCertErrorDetails",
+        browser.messageManager.sendAsyncMessage("CertErrorDetails",
                                                 { info: errorInfo });
         break;
 
