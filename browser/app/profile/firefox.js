ec821d28528c56d338865af9dd92eb83d41fe0e3
Nicholas McAnespy <ClassicNick@github.com>
2026-07-19 22:17:53 -0300

    Revert "Bug 1227361 - Remove checks for MOZ_SERVICES_SYNC from browser/. r=markh"
    This reverts commit 7f692225996305d95b09ff09b5aab499a2d5c1dc.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1101,6 +1101,7 @@ pref("browser.taskbar.lists.tasks.enabled", true);
 pref("browser.taskbar.lists.refreshInSeconds", 120);
 #endif
 
+#ifdef MOZ_SERVICES_SYNC
 // Info when outdated sync detected
 pref("services.sync.outdated.url", "http://www.palemoon.org/sync/update/");
 // The sync engines to use.
@@ -1173,6 +1174,7 @@ pref("services.sync.prefs.sync.security.tls.version.max", true);
 pref("services.sync.prefs.sync.signon.rememberSignons", true);
 pref("services.sync.prefs.sync.spellchecker.dictionary", true);
 pref("services.sync.prefs.sync.xpinstall.whitelist.required", true);
+#endif
 
 // Developer edition preferences
 #ifdef MOZ_DEV_EDITION
